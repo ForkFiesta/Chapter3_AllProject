938b24087a39190332132de0245a7980db24ac47
Raul Resendez <raul@cleansify.io>
2025-01-15 00:49:33 -0600

    Implement Simple Class Program

---
+++ SimpleClassExample/Motorcycle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Motorcycle
+    {
+        public int driverIntensity;
+
+        // New members to represent the name of the driver.
+        // Note that if there is no ambiguity, you can use the same name for a member as a parameter.
+        public string name;
+        public void SetDriverName(string name)
+        {
+            this.name = name;
+        }
+
+        public void PopAWheely()
+        {
+            for (int i = 0; i <= driverIntensity; i++)
+            {
+                Console.WriteLine("Yeeeeeee Haaaaaeewww!");
+            }
+        }
+
+        // Put back the default constructor, which will set all data members to default values.
+        public Motorcycle() { }
+        public Motorcycle(int intensity)
+        {
+            driverIntensity = intensity;
+        }
+
+
+
+
+
+    }
+}
---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using SimpleClassExample;
+
+Console.WriteLine("***** Fun with Class Types *****\n");
+
+// Allocate and configure a Car object.
+bool inDanger;
+Car myCar = new Car("Terry", 10, out inDanger);
+Motorcycle myBike = new Motorcycle(5);
+//myCar.petName = "Henry";
+//myCar.currSpeed = 10;
+
+// Speed up the car a few times and print out the new state.
+for (int i = 0; i <= 20; i++)
+{
+    myCar.SpeedUp(5);
+    myCar.PrintState();
+    Console.WriteLine("Is the car in danger? {0}", myCar.IsInDanger());
+}
+
+// Now make my Motorcycle known to the world.
+myBike.driverIntensity = 10;
+myBike.PopAWheely();
+
+Console.ReadLine();
